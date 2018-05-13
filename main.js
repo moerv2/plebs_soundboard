dfa605c8999380f24b7ee9dbd6bf258ab3ba6b9e
moerv2 <marvin.vey@freenet.de>
2018-05-13 03:57:27 +0200

    +Moving Image feature in JS

--- main.js
+++ main.js
@@ -26,4 +26,28 @@ window.onload = () => {
 
     /* Run once to apply the cached slider-setting */
     changeVolume();
+	
+	
+	$(document).ready(function(e) {
+    var width = $(document).width();
+
+    function goRight() {
+        $("#animate").animate({
+        left: width
+      }, 5000, function() {
+         setTimeout(goLeft, 50);
+      });
+    }
+    function goLeft() {
+        $("#animate").animate({
+        left: 0
+      }, 5000, function() {
+         setTimeout(goRight, 50);
+      });
+    }
+
+    setTimeout(goRight, 50);
+});
+	
 };
+
