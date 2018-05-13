a11c5d311f4b2443827c9c1ecb8468e2ef013e7f
Kai Niebes <tooawesometodie@googlemail.com>
2018-05-13 04:02:16 +0200

    + Moving Background

--- main.js
+++ main.js
@@ -1,53 +1,69 @@
 window.onload = () => {
-    /* Put the changeVolume function as callback for adjusting the volume slider */
-    document.querySelector('#volume-slider').addEventListener('input', changeVolume);
+	/* Put the changeVolume function as callback for adjusting the volume slider */
+	document.querySelector('#volume-slider').addEventListener('input', changeVolume);
 
-    /* Gets all the buttons, then iterates through them.
-    * Adds the ability to play any <audio> elements inside of the <button> element
-    * */
-    let buttons = document.querySelectorAll('button');
-    buttons.forEach((button) => {
-        button.addEventListener('click', (evt) => {
-            let children = evt.target.children;
-            for (let i = 0; i < children.length; i++) if (children.item(i).tagName === 'AUDIO') children.item(i).play();
-        })
-    });
+	/* Gets all the buttons, then iterates through them.
+	* Adds the ability to play any <audio> elements inside of the <button> element
+	* */
+	let buttons = document.querySelectorAll('button');
+	buttons.forEach((button) => {
+		button.addEventListener('click', (evt) => {
+			let children = evt.target.children;
+			for (let i = 0; i < children.length; i++) if (children.item(i).tagName === 'AUDIO') children.item(i).play();
+		})
+	});
 
-    /* Get the value of the volume slider
-    * Apply the value to each <audio> element
-    * */
-    function changeVolume() {
-        let volume = document.querySelector('#volume-slider').value;
-        let audios = document.querySelectorAll('audio');
-        for (let i = 0; i < audios.length; i++) {
-            audios[i].volume = parseFloat(volume);
-        }
-    }
+	/* Get the value of the volume slider
+	* Apply the value to each <audio> element
+	* */
+	function changeVolume() {
+		let volume = document.querySelector('#volume-slider').value;
+		let audios = document.querySelectorAll('audio');
+		for (let i = 0; i < audios.length; i++) {
+			audios[i].volume = parseFloat(volume);
+		}
+	}
 
-    /* Run once to apply the cached slider-setting */
-    changeVolume();
-	
-	
-	$(document).ready(function(e) {
-    var width = $(document).width();
+	/* Run once to apply the cached slider-setting */
+	changeVolume();
 
-    function goRight() {
-        $("#animate").animate({
-        left: width
-      }, 5000, function() {
-         setTimeout(goLeft, 50);
-      });
-    }
-    function goLeft() {
-        $("#animate").animate({
-        left: 0
-      }, 5000, function() {
-         setTimeout(goRight, 50);
-      });
-    }
+	/* Marvin Sample Box */
+	$(document).ready(function (e) {
+		let width = $(document).width();
 
-    setTimeout(goRight, 50);
-});
-	
+		function goRight() {
+			$("#animate").animate({
+				left: width
+			}, 5000, function () {
+				setTimeout(goLeft, 50);
+			});
+		}
+
+		function goLeft() {
+			$("#animate").animate({
+				left: 0
+			}, 5000, function () {
+				setTimeout(goRight, 50);
+			});
+		}
+
+		setTimeout(goRight, 50);
+	});
+
+	/* Moving Matrix Background */
+	function moveBackground() {
+		let body = document.body;
+		let offset = body.style.backgroundPositionY.replace('px', '');
+		if (offset < 168) {
+			offset++;
+		} else {
+			offset = 0;
+		}
+		body.style.backgroundPositionY = offset + 'px';
+		setTimeout(moveBackground, 16);
+	}
+
+	moveBackground();
+
 };
 
